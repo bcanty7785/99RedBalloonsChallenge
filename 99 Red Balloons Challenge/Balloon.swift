75890bbd55126c7d5c654fa9e59ef22442257aa7
briancanty <brian_canty@mba.berkeley.edu>
2015-05-07 19:40:08 -0700

    99 Red Balloons Challenge Final Code

---
+++ 99 Red Balloons Challenge/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99 Red Balloons Challenge
+//
+//  Created by Brian Canty on 4/13/15.
+//  Copyright (c) 2015 Brian Canty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage(named:"")
+}
--- 99 Red Balloons Challenge/ViewController.swift
+++ 99 Red Balloons Challenge/ViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonItem: UIImageView!
+    @IBOutlet weak var balloonsLabel: UILabel!
+    
+    var balloons:[Balloon] = []
+    
+    var currentIndex = 0
+    
     override func viewDidLoad() {
+        self.createBalloons()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +28,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBalloonButtonPressed(sender: UIBarButtonItem) {
+        let balloon = balloons[currentIndex]
+        
+        balloonsLabel.text = "\(balloon.number) balloon"
+        balloonItem.image = balloon.image
+        
+        currentIndex += 1
+    }
+    
+    func createBalloons () {
+        for var balloonCount = 0; balloonCount <= 99; ++balloonCount
+        {
+            var balloon = Balloon()
+            balloon.number = balloonCount
+            let randomNumber = Int(arc4random_uniform(UInt32(4)))
+            switch randomNumber {
+            case 1:
+                balloon.image = UIImage (named: "RedBalloon1.jpg")
+            case 2:
+                balloon.image = UIImage (named: "RedBalloon2.jpg")
+            case 3:
+                balloon.image = UIImage (named: "RedBalloon3.jpg")
+            default:
+                balloon.image = UIImage (named: "RedBalloon4.jpg")
+            }
+            self.balloons.append(balloon)
+        }
+        
+    }
 
 }
 
